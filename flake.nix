62e2ff09742dc28085586afee211f0e4ad0bfb62
Ingo Ruhnke <grumbel@gmail.com>
2022-02-27 00:53:42 +0100

    Add Nix flake build files

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Dump Gnome's binary keyrings into text format";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nix, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          gnome-keyring-raw = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "gnome-keyring-raw";
+            version = "0.1.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            nativeBuildInputs = with pkgs.python3.pkgs; [
+              flake8
+            ];
+            propagatedBuildInputs = with pkgs.python3.pkgs; [
+              pyyaml
+              pycrypto
+            ];
+           };
+        };
+        defaultPackage = packages.gnome-keyring-raw;
+      });
+}
