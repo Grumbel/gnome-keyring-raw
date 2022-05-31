c41bdffb0e2ce1ab46b5f084619368187bc7186b
Ingo Ruhnke <grumbel@gmail.com>
2022-05-31 18:49:51 +0200

    Update flake.nix to nixos-22.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Dump Gnome's binary keyrings into text format";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
